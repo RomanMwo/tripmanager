984583cfca41e942d0f81b362819e0ce389e2cc8
student16 <student16@pcoip.ki.agh.edu.pl>
2018-03-17 15:30:26 +0100

    step1

--- src/test/java/pl/edu/agh/mwo/TripManagerTest.java
+++ src/test/java/pl/edu/agh/mwo/TripManagerTest.java
@@ -1,6 +1,6 @@
 package pl.edu.agh.mwo;
  
 public class TripManagerTest {
-	
+
 
 }
---
+++ src/test/java/pl/edu/agh/mwo/TripManagerTestTest.java
@@ -0,0 +1,14 @@
+package pl.edu.agh.mwo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TripManagerTestTest {
+
+	@Test
+	public void test() {
+		assertEquals(2, 2);
+	}
+
+}
