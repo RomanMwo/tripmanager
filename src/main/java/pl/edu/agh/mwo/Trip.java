c15e6eb293a9a4962339a57015bef63306af8f52
romanchlebus <orlik313@gmail.com>
2018-04-05 20:26:31 +0200

    trip test

--- src/main/java/pl/edu/agh/mwo/Trip.java
+++ src/main/java/pl/edu/agh/mwo/Trip.java
@@ -3,15 +3,47 @@ package pl.edu.agh.mwo;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class Trip {
+public class Trip
+{
+	private String name;
+	private String description;
+	
+
+	public Trip(String name, String description)
+	{
+		this.name = name;
+		this.description = description;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
 
 	Collection<Photo> photos = new ArrayList<Photo>();
-	
-	public Collection<Photo> getPhotos() {
+
+	public Collection<Photo> getPhotos()
+	{
 		return photos;
 	}
 
-	public void addPhoto(Photo photo) {
+	public void addPhoto(Photo photo)
+	{
 		photos.add(photo);
 	}
 
--- src/test/java/pl/edu/agh/mwo/TripTest.java
+++ src/test/java/pl/edu/agh/mwo/TripTest.java
@@ -2,18 +2,73 @@ package pl.edu.agh.mwo;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.regex.Matcher;
+
+
 import org.junit.Test;
 
 public class TripTest {
+	
+	
+	@Test
+	public void getTripName()
+	{
+		Trip journey = new Trip("Hiszpania", "Wspaniala pogoda");
+		assertEquals("Hiszpania", journey.getName());
+	}
+	
+	@Test
+	public void getTripDescription()
+	{
+		Trip journey = new Trip("Hiszpania", "Wspaniala pogoda");
+		assertEquals("Wspaniala pogoda", journey.getDescription());
+	}
+	
+	@Test
+	public void setTripName()
+	{
+		Trip journey = new Trip("Hiszpania", "Wspaniala pogoda");
+		journey.setName("wlochy");
+		assertEquals("wlochy", journey.getName());
+	}
+	
+	@Test
+	public void setTripDescription()
+	{
+		Trip journey = new Trip("Hiszpania", "Wspaniala pogoda");
+		journey.setDescription("pogoda");
+		assertEquals("pogoda", journey.getDescription());
+	}
 
 	@Test
 	public void addPhoto() {
-		Trip trip = new Trip();
+		Trip trip = new Trip("Hiszpania", "Wspaniala pogoda");
 		Photo photo = new Photo("naj");
 		assertEquals(0, trip.getPhotos().size());
 		trip.addPhoto(photo);
 		assertEquals(1, trip.getPhotos().size());
 	}
 	
-
+	@Test
+	public void getPhot() {
+		Collection<Photo> newPhotos = new ArrayList<>();
+		Trip trip = new Trip("Hiszpania", "Wspaniala pogoda");
+		Photo photo = new Photo("naj");
+		trip.addPhoto(photo);
+		newPhotos.add(photo);
+		
+		 assertEquals(newPhotos, trip.getPhotos());
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testThrowsExeption() {
+		Trip trip = new Trip("Hiszpania", "Wspaniala pogoda");
+		Photo photo = new Photo("naj");
+		trip.addPhoto(photo);
+		((ArrayList<Photo>) trip.photos).get(2);
+	
+}
+	
 }
