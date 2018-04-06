58b5f8402b4b3ea5265fbfcb846af4acbe39a908
romanchlebus <orlik313@gmail.com>
2018-04-06 22:37:04 +0200

    final1

--- src/main/java/pl/edu/agh/mwo/TripManager.java
+++ src/main/java/pl/edu/agh/mwo/TripManager.java
@@ -1,18 +1,38 @@
 package pl.edu.agh.mwo;
 
-
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class TripManager
 {
 
+	private Collection<Trip> trips = new ArrayList<Trip>();
+
 	public void addTrip(Trip trip)
 	{
-		
-		
+		trips.add(trip);
+
 	}
 
-	
-	
-	
+	public Collection<Trip> getTrips()
+	{
+		return trips;
+	}
+
+	public void removeTrip(Trip trip)
+	{
+		trips.remove(trip);
+	}
+
+	public String findTrip(String nameTrip)
+	{
+		String results = null;
+		Trip trip = new Trip(nameTrip, "dowolnyNieistotnyDescription");
+		if (trip.getName() == nameTrip)
+			results = nameTrip;
+		else 
+			System.out.println("nie znalezlismy twojej wycieczki");
+		return results;
+	}
 
 }
--- src/test/java/pl/edu/agh/mwo/TripManagerTest.java
+++ src/test/java/pl/edu/agh/mwo/TripManagerTest.java
@@ -2,6 +2,10 @@ package pl.edu.agh.mwo;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.regex.Matcher;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,9 +18,35 @@ public class TripManagerTest {
 	        tripMan = new TripManager();
 	}
 	    @Test
-		public void addTripTest(Trip trip)
+		public void addTripTest()
 		{
 	    	Trip journey = new Trip("Hiszpania", "Wspaniala pogoda");
+	    	assertEquals(0, tripMan.getTrips().size());
 	    	tripMan.addTrip(journey);
+	    	assertEquals(1, tripMan.getTrips().size());
 		}
+		
+	    @Test
+	 		public void removeTripTest()
+	 		{
+	 	    	Trip journey = new Trip("Hiszpania", "Wspaniala pogoda");
+	 	    	Trip journey2 = new Trip("wlochy", "gorsza pogoda");
+	 	    	tripMan.addTrip(journey);
+	 	    	tripMan.addTrip(journey2);
+	 	    	tripMan.removeTrip(journey);
+	 	    	assertEquals(1, tripMan.getTrips().size());
+	 		}
+	    
+	    @Test
+ 		public void findTripTest()
+ 		{
+ 	    	Trip journey = new Trip("Hiszpania", "Wspaniala pogoda");
+ 	    	Trip journey2 = new Trip("wlochy", "gorsza pogoda");
+ 	    	tripMan.addTrip(journey);
+ 	    	tripMan.addTrip(journey2);
+ 	    	
+ 	    	assertEquals(journey2.getName(), tripMan.findTrip("wlochy") );
+ 		}
+	
+    
 }
